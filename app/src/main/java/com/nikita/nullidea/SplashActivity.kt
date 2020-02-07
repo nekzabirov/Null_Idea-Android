a3d26d4101abc3880a8acdb254802740a643ff90
nikita <nikita.zabirov@practicallogix.com>
2020-02-07 17:46:01 +0200

    Renamed MainActivity

--- app/src/main/java/com/nikita/nullidea/SplashActivity.kt
+++ app/src/main/java/com/nikita/nullidea/SplashActivity.kt
@@ -3,7 +3,7 @@ package com.nikita.nullidea
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
