1709905879ca900236b53930d56b78424d6144dc
nikita <nikita.zabirov@practicallogix.com>
2020-02-07 23:36:06 +0200

    Created api tools

---
+++ app/src/main/java/com/nikita/nullidea/unit/tool/ApiTools.kt
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2020.
+ * Nkita Knyazevkiy
+ * UA
+ */
+
+package com.nikita.nullidea.unit.tool
+
+import com.nikita.nullidea.unit.rest.ApiFactory
+
+object ApiTools {
+
+    private val apiFactory: ApiFactory = ApiFactory()
+
+    private val apiUrl = ""
+
+    fun <T> api(service: Class<T>): T {
+        return apiFactory
+            .buildRetrofit(apiUrl)
+            .create(service)
+    }
+}
