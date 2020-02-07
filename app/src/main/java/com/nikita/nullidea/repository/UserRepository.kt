a859b968ea3dcc7a3b101e7066227b333559fabd
nikita <nikita.zabirov@practicallogix.com>
2020-02-08 00:04:11 +0200

    Load firebase token func

--- app/src/main/java/com/nikita/nullidea/repository/UserRepository.kt
+++ app/src/main/java/com/nikita/nullidea/repository/UserRepository.kt
@@ -1,5 +1,39 @@
 package com.nikita.nullidea.repository
 
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.iid.FirebaseInstanceId
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+import kotlin.coroutines.suspendCoroutine
+
 class UserRepository {
+
+    suspend fun getFirebaseToken(): String = suspendCoroutine { continuation ->
+        FirebaseInstanceId.getInstance()
+            .instanceId
+            .addOnCompleteListener(
+                OnCompleteListener { task ->
+
+                if (!task.isSuccessful) {
+                    continuation.resumeWithException(
+                        IllegalArgumentException("Cannot load firebase token")
+                    )
+                    return@OnCompleteListener
+                }
+
+                // Get new Instance ID token
+                val token = task.result?.token
+
+                if (token == null) {
+                    continuation.resumeWithException(
+                        IllegalArgumentException("Firebase token is null")
+                    )
+                    return@OnCompleteListener
+                }
+
+                continuation.resume(token)
+            }
+            )
+    }
 
 }
