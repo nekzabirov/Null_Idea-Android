387a2a5bea5e3f0dddfceca0adf6abfd1b122108
nikita <nikita.zabirov@practicallogix.com>
2020-02-09 00:38:36 +0200

    Added log

--- app/src/main/java/com/nikita/nullidea/repository/TokenRepository.kt
+++ app/src/main/java/com/nikita/nullidea/repository/TokenRepository.kt
@@ -6,14 +6,19 @@
 
 package com.nikita.nullidea.repository
 
+import com.nikita.nullidea.TAG
 import com.nikita.nullidea.api.TokenApiService
 import com.nikita.nullidea.model.TokenModel
 import com.nikita.nullidea.unit.rest.AppRest
+import com.nikita.nullidea.unit.tool.MyLog
 import com.nikita.nullidea.unit.tool.PreferenceTools
 
 class TokenRepository {
 
     suspend fun accessToken(): TokenModel {
+
+        MyLog.d(TAG, "start load access token")
+
         val tokenModel = AppRest(
             "https://nullidea.eu.auth0.com/oauth/token",
             TokenApiService::class.java
