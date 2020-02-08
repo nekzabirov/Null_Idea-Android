c9be852aed4e4aca7f1d084207c097c6feeee5d9
nikita <nikita.zabirov@practicallogix.com>
2020-02-09 01:34:02 +0200

    Created sign in method

--- app/src/main/java/com/nikita/nullidea/repository/UserRepository.kt
+++ app/src/main/java/com/nikita/nullidea/repository/UserRepository.kt
@@ -1,5 +1,23 @@
 package com.nikita.nullidea.repository
 
+import com.nikita.nullidea.api.UserApiService
+import com.nikita.nullidea.api.UserRequestModel
+import com.nikita.nullidea.db.UserEntity
+import com.nikita.nullidea.model.BaseResponseModel
+import com.nikita.nullidea.unit.rest.AppRest
+
 class UserRepository: BaseRepository() {
+
+    private val userService: UserApiService by lazy {
+        AppRest(apiUrl, UserApiService::class.java).api()
+    }
+
+    suspend fun signIn(email: String, password: String): BaseResponseModel<UserEntity> {
+        val responseModel = userService.signIn(
+            UserRequestModel(email, password)
+        )
+
+        return responseModel
+    }
 
 }
