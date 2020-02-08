fbd4c6a49e31175221fc21f8ca1dd04173c4218b
nikita <nikita.zabirov@practicallogix.com>
2020-02-09 01:16:34 +0200

    Created base repository

---
+++ app/src/main/java/com/nikita/nullidea/model/BaseResponseModel.kt
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2020.
+ * Nkita Knyazevkiy
+ * UA
+ */
+
+package com.nikita.nullidea.model
+
+data class BaseResponseModel <T> (
+    val status: String,
+    val error: String,
+    val data: T
+)
